7850d1d2243d017a2bcd1b0cee5f80ba58e6fa24
infinity <Infinitay@users.noreply.github.com>
2024-07-08 23:26:52 -0400

    Fixes plugin not working due to missing raw message module

--- InAppNotifications/InAppNotifications.plugin.js
+++ InAppNotifications/InAppNotifications.plugin.js
@@ -21,16 +21,16 @@ const config = {
         ],
     github_raw:
       "https://raw.githubusercontent.com/QWERTxD/BetterDiscordPlugins/main/InAppNotifications/InAppNotifications.plugin.js",
-    version: "1.1.5",
+    version: "1.1.6",
     description:
       "Displays notifications such as new messages, friends added in Discord.",
 	},
   changelog: [
     {
-      "title": "Discriminators",
-      "type": "added",
+      "title": "Fixed",
+      "type": "fixed",
       "items": [
-        "Modified how usernames and display names are displayed in notifications.",
+        "Fixes plugin not working due to missing raw message module",
       ]
     }
   ],
@@ -208,7 +208,7 @@ const config = {
 
       const ChannelTypes = Webpack.getModule(Webpack.Filters.byProps("GUILD_TEXT"), { searchExports: true });
       const MuteStore = WebpackModules.getByProps("isSuppressEveryoneEnabled");
-      const isMentioned = Webpack.getModule(x=>x.isRawMessageMentioned)
+      const isRawMessageMentioned = Webpack.getModule(Webpack.Filters.byStrings("userId", "channelId", "mentionEveryone", "mentionUsers", "mentionRoles", "suppressEveryone", "suppressRoles"), { searchExports: true });
       const Markdown = WebpackModules.getByProps("parse", "parseTopic");
       const GuildStore = Webpack.getStore("GuildStore")
       const AckUtils = { ack: Webpack.getModule(Webpack.Filters.byStrings("CHANNEL_ACK"), { searchExports: true }) };
@@ -993,7 +993,7 @@ const config = {
             message.guild_id || "@me"
           );
           if (MuteStore.allowAllMessages(channel)) return true;
-          const SomethingHereShrug = isMentioned.isRawMessageMentioned(
+          const SomethingHereShrug = isRawMessageMentioned(
             {
               rawMessage: message,
               userId: UserStore.getCurrentUser().id,
